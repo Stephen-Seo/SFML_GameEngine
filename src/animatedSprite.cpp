f0ebcaa282f067c3e351cc2f1cd64374d56f052d
Stephen Seo <seo.disparate@gmail.com>
2014-07-04 10:35:04 -0400

    Added support for sprite animations

---
+++ src/animatedSprite.cpp
@@ -0,0 +1,136 @@
+
+#include "animatedSprite.hpp"
+
+AnimatedSprite::AnimatedSprite() :
+sprite(),
+textureLoaded(false),
+currentState(0),
+currentFrame(0),
+reversed(false),
+rectDirty(true),
+type(CYCLE)
+{
+}
+
+AnimatedSprite::AnimatedSprite(const sf::Texture& texture) :
+sprite(texture),
+textureLoaded(true),
+currentState(0),
+currentFrame(0),
+reversed(false),
+rectDirty(true),
+type(CYCLE)
+{
+}
+
+void AnimatedSprite::setTexture(const sf::Texture& texture)
+{
+    sprite.setTexture(texture, true);
+    textureLoaded = true;
+    if(currentState < spriteMapping.size() && currentFrame < spriteMapping[currentState].size())
+    {
+        rect.left = spriteMapping[currentState][currentFrame].first;
+        rect.top = spriteMapping[currentState][currentFrame].second;
+        rectDirty = true;
+    }
+}
+
+void AnimatedSprite::addState(const SpriteState& state)
+{
+    spriteMapping.push_back(state);
+}
+
+void AnimatedSprite::setState(int ID)
+{
+    setState(ID, 0);
+}
+
+void AnimatedSprite::setState(int ID, int frame)
+{
+    if(ID >= 0 && ID < spriteMapping.size() && frame >= 0 && frame < spriteMapping[ID].size())
+    {
+        currentState = ID;
+        currentFrame = frame;
+        rect.left = spriteMapping[currentState][currentFrame].first;
+        rect.top = spriteMapping[currentState][currentFrame].second;
+        rectDirty = true;
+    }
+}
+
+void AnimatedSprite::setSpriteSize(int width, int height)
+{
+    rectDirty = true;
+    rect.width = width;
+    rect.height = height;
+}
+
+void AnimatedSprite::setSpriteSize(sf::Vector2i size)
+{
+    setSpriteSize(size.x, size.y);
+}
+
+void AnimatedSprite::setTimePerFrame(float seconds)
+{
+    timePerFrame = sf::seconds(seconds);
+}
+
+void AnimatedSprite::setTimePerFrame(int milliseconds)
+{
+    timePerFrame = sf::milliseconds(milliseconds);
+}
+
+void AnimatedSprite::setRotationType(RotationType type, bool resetReversed)
+{
+    this->type = type;
+    if(resetReversed)
+    {
+        reversed = false;
+    }
+}
+
+void AnimatedSprite::updateSprite(sf::Time dt)
+{
+    time += dt;
+    while(time > timePerFrame)
+    {
+        switch(type)
+        {
+        case CYCLE:
+            currentFrame = (currentFrame + 1) % spriteMapping[currentState].size();
+            break;
+        case REVERSE:
+            currentFrame = currentFrame + (reversed ? -1 : 1);
+            if(currentFrame < 0 || currentFrame >= spriteMapping[currentState].size())
+            {
+                currentFrame = currentFrame + (reversed ? 2 : -2);
+                reversed = !reversed;
+            }
+            break;
+        default:
+            break;
+        }
+
+        rect.left = spriteMapping[currentState][currentFrame].first;
+        rect.top = spriteMapping[currentState][currentFrame].second;
+        rectDirty = true;
+        time -= timePerFrame;
+    }
+}
+
+bool AnimatedSprite::isInitialized()
+{
+    return textureLoaded && spriteMapping.size() > 0;
+}
+
+void AnimatedSprite::draw(sf::RenderTarget& target, sf::RenderStates states)
+{
+    states.transform = getTransform() * states.transform;
+
+    if(rectDirty)
+    {
+        sprite.setTextureRect(rect);
+        rectDirty = false;
+    }
+
+    target.draw(sprite, states);
+}
---
+++ src/animatedSprite.hpp
@@ -0,0 +1,61 @@
+
+#ifndef ANIMATED_SPRITE_HPP
+#define ANIMATED_SPRITE_HPP
+
+#include <vector>
+#include <utility>
+
+#include <SFML/Graphics.hpp>
+
+class AnimatedSprite : public sf::Transformable, public sf::Drawable
+{
+public:
+    typedef std::vector<std::pair<int, int> > SpriteState;
+
+    enum RotationType
+    {
+        CYCLE,
+        REVERSE
+    };
+
+    AnimatedSprite();
+    AnimatedSprite(const sf::Texture& texture);
+
+    void setTexture(const sf::Texture& texture);
+
+    void addState(const SpriteState& state);
+
+    void setState(int ID);
+    void setState(int ID, int frame);
+
+    void setSpriteSize(int width, int height);
+    void setSpriteSize(sf::Vector2i size);
+
+    void setTimePerFrame(float seconds);
+    void setTimePerFrame(int milliseconds);
+
+    void setRotationType(RotationType type, bool resetReversed = false);
+
+    void updateSprite(sf::Time dt);
+private:
+    sf::Sprite sprite;
+    bool textureLoaded;
+    int currentState;
+    int currentFrame;
+    bool reversed;
+
+    sf::IntRect rect;
+    bool rectDirty;
+
+    RotationType type;
+
+    sf::Time time;
+    sf::Time timePerFrame;
+
+    std::vector<SpriteState> spriteMapping;
+
+    bool isInitialized();
+    void draw(sf::RenderTarget& target, sf::RenderStates states);
+};
+
+#endif
